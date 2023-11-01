aa18bcec1a932f341f85aab20c9053a867e67fd7
mpollard33 <mpollard33@gmail.com>
2023-11-01 14:01:32 -0600

    updated seed.js seed function

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,39 +1,42 @@
 // Import the 'prisma' module from a file located in the parent directory ('../prisma').
-const prisma = require('../prisma');
+const prisma = require ('../prisma');
 
 // Import author and book data from external files.
-const authorNames = require('../data/authors');
-const bookTitles = require('../data/books');
+const authorNames = require ('../data/authors');
+const bookTitles = require ('../data/books');
 
 // Define a seed function that populates the database with authors and books.
 const seed = async () => {
   // Create an array of author promises.
-  const authorPromises = authorNames.map(async (authorName) => {
-    return prisma.author.create({
+  const authorPromises = authorNames.map (async authorName => {
+    return prisma.author.create ({
       data: {
         name: authorName,
       },
     });
   });
+  const authors = await Promise.all (authorPromises);
 
-  // Create an array of book promises.
-  const bookPromises = bookTitles.map(async (bookTitle) => {
-    return prisma.book.create({
+  const bookPromises = bookTitles.map (async (title, index) => {
+    return prisma.book.create ({
       data: {
-        title: bookTitle,
+        title,
+        author: {
+          connect: {
+            id: authors[index].id,
+          },
+        },
       },
     });
   });
-
-  // Wait for all author and book promises to complete.
-  await Promise.all([...authorPromises, ...bookPromises]);
+  await Promise.all (bookPromises);
 };
 
 // Call the 'seed' function, which will execute the database seeding process.
-seed()
-  .then(async () => await prisma.$disconnect()) // When 'seed' is done, disconnect from the database.
-  .catch(async (e) => {
-    console.error(e); // If an error occurs during seeding, print it to the console.
-    await prisma.$disconnect(); // Ensure that we disconnect from the database.
-    process.exit(1); // Exit the program with an error code (1).
-});
+seed ()
+  .then (async () => await prisma.$disconnect ()) // When 'seed' is done, disconnect from the database.
+  .catch (async e => {
+    console.error (e); // If an error occurs during seeding, print it to the console.
+    await prisma.$disconnect (); // Ensure that we disconnect from the database.
+    process.exit (1); // Exit the program with an error code (1).
+  });
