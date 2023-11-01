50ae6910dcbbbe8a6a737043d6bb45b8f4742d8f
mpollard33 <mpollard33@gmail.com>
2023-11-01 13:45:13 -0600

    generated comments for reference on seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,47 +1,39 @@
 // Import the 'prisma' module from a file located in the parent directory ('../prisma').
-const prisma = require ('../prisma');
+const prisma = require('../prisma');
 
-const authorNames = require ('../data/authors');
-const bookTitles = require ('../data/books');
+// Import author and book data from external files.
+const authorNames = require('../data/authors');
+const bookTitles = require('../data/books');
 
+// Define a seed function that populates the database with authors and books.
 const seed = async () => {
-  const authorPromises = authorNames.map (async authorName => {
-    return prisma.author.create ({
+  // Create an array of author promises.
+  const authorPromises = authorNames.map(async (authorName) => {
+    return prisma.author.create({
       data: {
         name: authorName,
       },
     });
   });
 
-  const bookPromises = bookTitles.map (async bookTitle => {
-    return prisma.book.create ({
+  // Create an array of book promises.
+  const bookPromises = bookTitles.map(async (bookTitle) => {
+    return prisma.book.create({
       data: {
         title: bookTitle,
       },
     });
   });
-  await Promise.all ([...authorPromises, ...bookPromises]);
+
+  // Wait for all author and book promises to complete.
+  await Promise.all([...authorPromises, ...bookPromises]);
 };
-// Call the 'seed' function, which will execute the code inside it. It returns a Promise.
-seed ()
-  .then (async () => await prisma.$disconnect ()) // When 'seed' is done, disconnect from the database.
-  .catch (async e => {
-    console.error (e); // If an error occurs, print it to the console.
-    await prisma.$disconnect (); // Ensure that we disconnect from the database.
-    process.exit (1); // Exit the program with an error code (1).
-  });
 
-/*  -- CreateTable
-CREATE TABLE "Author" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "Book" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "authorId" INTEGER NOT NULL,
-    CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
-*/
+// Call the 'seed' function, which will execute the database seeding process.
+seed()
+  .then(async () => await prisma.$disconnect()) // When 'seed' is done, disconnect from the database.
+  .catch(async (e) => {
+    console.error(e); // If an error occurs during seeding, print it to the console.
+    await prisma.$disconnect(); // Ensure that we disconnect from the database.
+    process.exit(1); // Exit the program with an error code (1).
+});
