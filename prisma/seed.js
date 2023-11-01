6a318599ab2b4c923365888810d07d9d9551d4fc
mpollard33 <mpollard33@gmail.com>
2023-11-01 13:12:48 -0600

    created authors.js & finished seed function

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,26 @@
 // Import the 'prisma' module from a file located in the parent directory ('../prisma').
-const prisma = require('../prisma');
+const prisma = require ('../prisma');
+
+const authorNames = require ('../data/authors');
 
 const seed = async () => {
-
+  authorNames.map (async author => {
+    const author = await prisma.user.create ({
+      data: {
+        name: author,
+      },
+    });
+  });
 };
-
 // Call the 'seed' function, which will execute the code inside it. It returns a Promise.
-seed()
-  .then(async () => await prisma.$disconnect()) // When 'seed' is done, disconnect from the database.
-  .catch(async (e) => {
-    console.error(e); // If an error occurs, print it to the console.
-    await prisma.$disconnect(); // Ensure that we disconnect from the database.
-    process.exit(1); // Exit the program with an error code (1).
+seed ()
+  .then (async () => await prisma.$disconnect ()) // When 'seed' is done, disconnect from the database.
+  .catch (async e => {
+    console.error (e); // If an error occurs, print it to the console.
+    await prisma.$disconnect (); // Ensure that we disconnect from the database.
+    process.exit (1); // Exit the program with an error code (1).
   });
 
-  
 /*  -- CreateTable
 CREATE TABLE "Author" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
